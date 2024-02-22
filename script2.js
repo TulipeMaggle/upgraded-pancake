a206abe4c16901cf461184a27b5883c92a34b3de
TulipeMaggle <tulipemaggle@gmail.com>
2024-02-22 21:27:37 +0100

    Yo

--- script2.js
+++ script2.js
@@ -2,25 +2,23 @@
 const buttonswipe = document.querySelector("#button-swipe")
 const text2 = document.querySelector(".text2")
 const text = document.querySelector(".text")
-
-
-
-buttonswipe.addEventListener("click", () => {    
-        text.classList.add("cachertexte")
-      
-    })
-    
 var variable = false;
 
 buttonswipe.addEventListener("click", () => {
   if (variable > 0) {
-    text.classList.add("cachertexte")
+    text.classList.add("animationslide")
+    text.classList.remove("animationslide2")
+    text2.classList.add("animationslide2")
     text2.classList.remove("cachertexte")
+    text2.classList.remove("animationslide")
     console.log(variable + "dddd");
     variable = 0
   } else {
-    text.classList.remove("cachertexte")
-    text2.classList.add("cachertexte")
+    text.classList.remove("animationslide")
+    text2.classList.remove("animationslide2")
+    text.classList.add("animationslide2")
+    text2.classList.add("animationslide")
+  
     variable = 1
   }
 })
