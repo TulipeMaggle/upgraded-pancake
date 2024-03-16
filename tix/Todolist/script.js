d38942ab61754516dab9189d9e3b9843266bba30
TulipeMaggle <tulipemaggle@gmail.com>
2024-03-16 03:26:14 +0100

    d

--- tix/Todolist/script.js
+++ tix/Todolist/script.js
@@ -1,3 +1,42 @@
-import {fetchserver} from '../Todolist/script/functions.js'
+function createArticle(post) {
+    const article = document.createElement("div")
+    const h1 = document.createElement("h1")
+    h1.innerText = post.title
+    article.append(h1)
+    return article
+}
 
-fetchserver("https://jsonplaceholder.typicode.com/comments")
+function createElementAvectext(tagName, text) {
+    const element = document.createElement(tagName)
+    element.innertext = text
+    return element
+}
+
+async function main() {
+    const conteneur = document.querySelector("#article")
+    console.log(conteneur);
+    try {
+        const r = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5", {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+        if (r.ok !== true) {
+            throw new Error("y'a un problème");
+        }
+
+        const response = await r.json()
+        console.log(response);
+        for (let post of response) {
+            console.log(conteneur.append(createArticle(post)));
+            conteneur.append(createArticle(post))
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
+}
+main()
--- tix/Todolist/script/functions.js
+++ tix/Todolist/script/functions.js
@@ -1,8 +1,9 @@
-export async function fetchserver(url) {
-    const r = await fetch("https://jsonplaceholder.typicode.com/posts")
-    const response = await r.json()
-    console.log(response);
-    response.forEach(element => {
-        console.count(element);
-    });
-}
+/**
+ * 
+ * @param {string} tagName 
+ */
+export function createElement(tagName, text) {
+    const article = document.createElement(tagName)
+    article.innertext = text
+    return article
+} 
