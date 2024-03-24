64f8a3a4dabb72522afc94a67f5a26b8282ee9bb
TulipeMaggle <tulipemaggle@gmail.com>
2024-03-24 01:11:56 +0100

    d

---
+++ tix/Projet-PE/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>test dqzdq</title>
+</head>
+
+<body>
+	<form action="Stagiaire">
+		<label for="Stagiaire">
+			<input type="text" name="nom" placeholder="Entrez votre nom" />
+			<input type="submit" value="Envoyer" />
+		</label>
+	</form>
+</body>
+
+</html>
--- tix/Projet-PE/script.js
+++
@@ -1,4 +0,0 @@
-import fs from 'node:fs/promises'
-
-const content = await fs.readFile('yo.txt', { encoding: 'utf8' })
-console.log(content)
